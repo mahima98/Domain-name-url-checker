82bbb019ef70962c7f98928774a4fc49f7246cc0
Mahima <mahimaramgolam98@gmail.com>
2021-06-08 19:41:54 +0400

    added input box

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,35 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="container mx-auto bg-red-200">
+      <div class="flex flex-col justify-center items-center h-screen">
+        <div class="bg-blue-300 p-10 flex flex-col gap-4 rounded-lg">
+          <div>
+            <div class="text-lg font-extrabold">Main url: </div>
+            <input type="text" class="p-2">
+          </div>
+          <div>
+            <div class="text-lg font-extrabold">Custom url: </div>
+            <input v-model="nameInput" type="text" class="p-2">
+          </div>
+          <button class="bg-black text-white p-2">Check</button>
+          <div class="errorOutput"></div>
+          <div class="output"></div>
+        </div>
+      </div>
     </div>
-    <router-view />
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  },
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  methods: {
+  }
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
