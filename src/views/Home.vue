49191c34cc5f792159d2e07d63aa7ab627a333e0
Mahima <mahimaramgolam98@gmail.com>
2021-06-08 19:13:09 +0400

    test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    hiiiiiiiiiii
+    hiiiiiiiiiiiii
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
