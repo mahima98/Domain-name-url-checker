3ac4fbfd117e95328081ea4b1ac6a6e3c00e72ec
Mahima <mahimaramgolam98@gmail.com>
2021-06-09 19:36:31 +0400

    Added display URL button on input a URL

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,14 @@
         <div class="bg-blue-300 p-10 flex flex-col gap-4 rounded-lg">
           <div>
             <div class="text-lg font-extrabold">Main url: </div>
-            <input type="text" class="p-2">
+            <input type="text" v-model="mainUrl" class="p-2 text-gray-400">
           </div>
           <div>
             <div class="text-lg font-extrabold">Custom url: </div>
-            <input v-model="nameInput" type="text" class="p-2">
+            <input v-model="customUrl" type="text" class="p-2">
           </div>
-          <button class="bg-black text-white p-2">Check</button>
-          <div class="errorOutput"></div>
+          <button class="bg-black text-white p-2" v-on:click="onDisplayInputUrl()">Display Input URL</button>
+          <div class="errorOutput">{{this.outputCustomUrl}}</div>
           <div class="output"></div>
         </div>
       </div>
@@ -25,11 +25,16 @@
 export default {
   data() {
     return {
-      
+      mainUrl: 'http://mahima.ramgolam.com',
+      outputCustomUrl: null,
+
     }
   },
 
   methods: {
+    onDisplayInputUrl() {
+      this.outputCustomUrl = this.customUrl
+    }
   }
 }
 </script>
